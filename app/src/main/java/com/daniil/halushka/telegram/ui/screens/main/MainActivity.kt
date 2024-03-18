31ce62e0c5bd97712bfe22d6b0f2be949bb05bf3
Daniil Halushka <popolzen22@gmail.com>
2024-03-18 23:20:09 +0300

    Refactor MainActivity.kt. Create AppDrawer

--- app/src/main/java/com/daniil/halushka/telegram/ui/screens/main/MainActivity.kt
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/main/MainActivity.kt
@@ -1,27 +1,18 @@
 package com.daniil.halushka.telegram.ui.screens.main
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.daniil.halushka.telegram.R
 import com.daniil.halushka.telegram.databinding.ActivityMainBinding
 import com.daniil.halushka.telegram.ui.screens.chat.ChatFragment
-import com.daniil.halushka.telegram.ui.screens.settings.SettingsFragment
-import com.mikepenz.materialdrawer.AccountHeader
-import com.mikepenz.materialdrawer.AccountHeaderBuilder
-import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
-import com.mikepenz.materialdrawer.model.DividerDrawerItem
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import com.daniil.halushka.telegram.ui.screens.util.AppDrawer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var moduleDrawer: Drawer
-    private lateinit var moduleHeader: AccountHeader
     private lateinit var moduleToolbar: Toolbar
+
+    private lateinit var moduleAppDrawer: AppDrawer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,103 +28,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun initializeFields() {
         moduleToolbar = binding.mainToolbar
+        moduleAppDrawer = AppDrawer(
+            mainActivity = this,
+            toolbar = moduleToolbar
+        )
     }
 
     private fun initializeFunctionality() {
+        moduleAppDrawer.create()
         setSupportActionBar(moduleToolbar)
         supportFragmentManager.beginTransaction()
             .replace(R.id.dataContainer, ChatFragment()).commit()
-        createHeader()
-        createDrawer()
-    }
-
-    private fun createHeader() {
-        moduleHeader = AccountHeaderBuilder()
-            .withActivity(this)
-            .withHeaderBackground(R.drawable.header)
-            .addProfiles(
-                ProfileDrawerItem()
-                    .withName("Daniil Halushka")
-                    .withEmail("test@gmail.com")
-            ).build()
-    }
-
-    private fun createDrawer() {
-        moduleDrawer = DrawerBuilder()
-            .withActivity(this)
-            .withToolbar(moduleToolbar)
-            .withAccountHeader(moduleHeader)
-            .withActionBarDrawerToggle(true)
-            .withSelectedItem(-1)
-            .addDrawerItems(
-                PrimaryDrawerItem()
-                    .withIdentifier(100)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.create_group)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_create_groups),
-                PrimaryDrawerItem()
-                    .withIdentifier(101)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.create_secret_chat)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_secret_chat),
-                PrimaryDrawerItem()
-                    .withIdentifier(102)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.create_channel)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_create_channel),
-                PrimaryDrawerItem()
-                    .withIdentifier(103)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.contacts)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_contacts),
-                PrimaryDrawerItem()
-                    .withIdentifier(104)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.calls)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_phone),
-                PrimaryDrawerItem()
-                    .withIdentifier(105)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.saved_messages)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_favorites),
-                PrimaryDrawerItem()
-                    .withIdentifier(106)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.settings)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_settings),
-                DividerDrawerItem(),
-                PrimaryDrawerItem()
-                    .withIdentifier(107)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.add_friends)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_invate),
-                PrimaryDrawerItem()
-                    .withIdentifier(108)
-                    .withIconTintingEnabled(true)
-                    .withName(R.string.faq)
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_help),
-            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
-                override fun onItemClick(
-                    view: View?,
-                    position: Int,
-                    drawerItem: IDrawerItem<*>
-                ): Boolean {
-                    when (position) {
-                        7 -> supportFragmentManager.beginTransaction()
-                            .addToBackStack(null)
-                            .replace(R.id.dataContainer, SettingsFragment()).commit()
-                    }
-                    return false
-                }
-            }).build()
     }
 }
---
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/util/AppDrawer.kt
@@ -0,0 +1,118 @@
+package com.daniil.halushka.telegram.ui.screens.util
+
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.daniil.halushka.telegram.R
+import com.daniil.halushka.telegram.ui.screens.settings.SettingsFragment
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+
+class AppDrawer(
+    val mainActivity: AppCompatActivity,
+    private val toolbar: Toolbar
+) {
+    private lateinit var moduleDrawer: Drawer
+    private lateinit var moduleHeader: AccountHeader
+
+    fun create(){
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createHeader() {
+        moduleHeader = AccountHeaderBuilder()
+            .withActivity(mainActivity)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem()
+                    .withName("Daniil Halushka")
+                    .withEmail("test@gmail.com")
+            ).build()
+    }
+
+    private fun createDrawer() {
+        moduleDrawer = DrawerBuilder()
+            .withActivity(mainActivity)
+            .withToolbar(toolbar)
+            .withAccountHeader(moduleHeader)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .addDrawerItems(
+                PrimaryDrawerItem()
+                    .withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.create_group)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_groups),
+                PrimaryDrawerItem()
+                    .withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.create_secret_chat)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_secret_chat),
+                PrimaryDrawerItem()
+                    .withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.create_channel)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_channel),
+                PrimaryDrawerItem()
+                    .withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.contacts)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_contacts),
+                PrimaryDrawerItem()
+                    .withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.calls)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_phone),
+                PrimaryDrawerItem()
+                    .withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.saved_messages)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_favorites),
+                PrimaryDrawerItem()
+                    .withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.settings)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_settings),
+                DividerDrawerItem(),
+                PrimaryDrawerItem()
+                    .withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.add_friends)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_invate),
+                PrimaryDrawerItem()
+                    .withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.faq)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_help),
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    when (position) {
+                        7 -> mainActivity.supportFragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.dataContainer, SettingsFragment()).commit()
+                    }
+                    return false
+                }
+            }).build()
+    }
+}
