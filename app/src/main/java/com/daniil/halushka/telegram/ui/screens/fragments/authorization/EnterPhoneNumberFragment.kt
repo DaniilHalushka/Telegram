bd1c1dbebbe26760101047b522568437713625cb
Daniil Halushka <popolzen22@gmail.com>
2024-03-25 20:10:31 +0300

    Start authorization fragment

---
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/authorization/EnterPhoneNumberFragment.kt
@@ -0,0 +1,9 @@
+package com.daniil.halushka.telegram.ui.screens.fragments.authorization
+
+import androidx.fragment.app.Fragment
+import com.daniil.halushka.telegram.R
+
+class EnterPhoneNumberFragment : Fragment(R.layout.fragment_enter_phone_number) {
+
+
+}
