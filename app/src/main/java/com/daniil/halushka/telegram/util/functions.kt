b3b637a0bb00ba605fa7b42a75b70a63ee8c4057
Daniil Halushka <popolzen22@gmail.com>
2024-04-16 18:25:50 +0300

    Fix bug with no view founding

--- app/src/main/java/com/daniil/halushka/telegram/util/functions.kt
+++ app/src/main/java/com/daniil/halushka/telegram/util/functions.kt
@@ -58,12 +58,12 @@ fun Fragment.replaceParentFragment(fragment: Fragment, dataContainer: Int) {
 }
 
 //*TODO* если метод не понадобится, то потом удалить
-fun Fragment.replaceChildFragment(fragment: Fragment, dataContainer: Int) {
-    this.childFragmentManager.beginTransaction()
-        .addToBackStack(null)
-        .replace(dataContainer, fragment)
-        .commit()
-}
+//fun Fragment.replaceChildFragment(fragment: Fragment, dataContainer: Int) {
+//    this.getChildFragmentManager().beginTransaction() //Поправил тут на getChildFragmentManager
+//        .addToBackStack(null)
+//        .replace(dataContainer, fragment)
+//        .commit()
+//}
 
 fun hideKeyboard() {
     val inputManager: InputMethodManager = APP_ACTIVITY
