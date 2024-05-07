608f8bb25f2ac84c86431b3ea59bc010980c0b9b
Daniil Halushka <popolzen22@gmail.com>
2024-05-07 20:27:36 +0300

    Add todo

--- app/src/main/java/com/daniil/halushka/telegram/util/AppVoiceRecorder.kt
+++ app/src/main/java/com/daniil/halushka/telegram/util/AppVoiceRecorder.kt
@@ -3,15 +3,15 @@ package com.daniil.halushka.telegram.util
 class AppVoiceRecorder {
     companion object {
         fun startRecord() {
-
+            //TODO сделать
         }
 
         fun stopRecord(onSuccess: () -> Unit) {
-
+            //TODO сделать
         }
 
         fun realiseRecorder() {
-
+            //TODO сделать
         }
     }
 }
