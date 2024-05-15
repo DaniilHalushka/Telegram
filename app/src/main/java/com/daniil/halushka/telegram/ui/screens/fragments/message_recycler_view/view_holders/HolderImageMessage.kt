de6ee42b1766fa6554e208f8e9b2bfc184156911
Daniil Halushka <popolzen22@gmail.com>
2024-05-15 23:40:13 +0300

    Create holder for voice message. Needs to fix bindings in holders

--- app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/message_recycler_view/view_holders/HolderImageMessage.kt
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/message_recycler_view/view_holders/HolderImageMessage.kt
@@ -5,15 +5,38 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.daniil.halushka.telegram.database.CURRENT_UID
 import com.daniil.halushka.telegram.databinding.MessageItemImageBinding
+import com.daniil.halushka.telegram.ui.screens.fragments.message_recycler_view.view.MessageView
+import com.daniil.halushka.telegram.util.asTime
+import com.daniil.halushka.telegram.util.downloadAndSetImage
 
 class HolderImageMessage(view: View) : RecyclerView.ViewHolder(view) {
     private lateinit var itemBinding: MessageItemImageBinding
 
-    val blockReceivedImageMessage: ConstraintLayout = itemBinding.blockReceivedImageMessage
-    val blockUserImageMessage: ConstraintLayout = itemBinding.blockUserImageMessage
-    val chatUserImage: ImageView = itemBinding.chatUserImage
-    val chatReceivedImage: ImageView = itemBinding.chatReceivedImage
-    val chatUserImageMessageTime: TextView = itemBinding.chatUserImageMessageTime
-    val chatReceivedImageMessageTime: TextView = itemBinding.chatReceivedImageMessageTime
+    private val blockReceivedImageMessage: ConstraintLayout = itemBinding.blockReceivedImageMessage
+    private val blockUserImageMessage: ConstraintLayout = itemBinding.blockUserImageMessage
+    private val chatUserImage: ImageView = itemBinding.chatUserImage
+    private val chatReceivedImage: ImageView = itemBinding.chatReceivedImage
+    private val chatUserImageMessageTime: TextView = itemBinding.chatUserImageMessageTime
+    private val chatReceivedImageMessageTime: TextView = itemBinding.chatReceivedImageMessageTime
+
+    fun drawMessageImage(
+        singleChatHolder: HolderImageMessage,
+        view: MessageView
+    ) {
+        if (view.from == CURRENT_UID) {
+            singleChatHolder.blockReceivedImageMessage.visibility = View.GONE
+            singleChatHolder.blockUserImageMessage.visibility = View.VISIBLE
+            singleChatHolder.chatUserImage.downloadAndSetImage(view.fileUrl)
+            singleChatHolder.chatUserImageMessageTime.text =
+                view.timeStamp.asTime()
+        } else {
+            singleChatHolder.blockReceivedImageMessage.visibility = View.VISIBLE
+            singleChatHolder.blockUserImageMessage.visibility = View.GONE
+            singleChatHolder.chatReceivedImage.downloadAndSetImage(view.fileUrl)
+            singleChatHolder.chatReceivedImageMessageTime.text =
+                view.timeStamp.asTime()
+        }
+    }
 }
--- app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/message_recycler_view/view_holders/HolderTextMessage.kt
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/message_recycler_view/view_holders/HolderTextMessage.kt
@@ -4,7 +4,10 @@ import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.daniil.halushka.telegram.database.CURRENT_UID
 import com.daniil.halushka.telegram.databinding.MessageItemTextBinding
+import com.daniil.halushka.telegram.ui.screens.fragments.message_recycler_view.view.MessageView
+import com.daniil.halushka.telegram.util.asTime
 
 class HolderTextMessage(view: View) : RecyclerView.ViewHolder(view) {
     private lateinit var itemBinding: MessageItemTextBinding
@@ -15,4 +18,26 @@ class HolderTextMessage(view: View) : RecyclerView.ViewHolder(view) {
     val blockReceivedMessage: ConstraintLayout = itemBinding.blockReceivedMessage
     val chatReceivedMessage: TextView = itemBinding.chatReceivedMessage
     val chatReceivedMessageTime: TextView = itemBinding.chatReceivedMessageTime
+
+    fun drawMessageText(
+        singleChatHolder: HolderTextMessage,
+        view: MessageView
+    ) {
+
+        if (view.from == CURRENT_UID) {
+            singleChatHolder.blockUserMessage.visibility = View.VISIBLE
+            singleChatHolder.blockReceivedMessage.visibility = View.GONE
+
+            singleChatHolder.chatUserMessage.text = view.text
+            singleChatHolder.chatUserMessageTime.text =
+                view.timeStamp.asTime()
+        } else {
+            singleChatHolder.blockUserMessage.visibility = View.GONE
+            singleChatHolder.blockReceivedMessage.visibility = View.VISIBLE
+
+            singleChatHolder.chatReceivedMessage.text = view.text
+            singleChatHolder.chatReceivedMessageTime.text =
+                view.timeStamp.asTime()
+        }
+    }
 }
