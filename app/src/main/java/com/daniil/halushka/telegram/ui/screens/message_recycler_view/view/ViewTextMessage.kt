33b7ed0670e81c166c51169230cf18797eedfa33
Daniil Halushka <popolzen22@gmail.com>
2024-05-17 19:26:42 +0300

    Refactor SingleChatAdapter. Create MessageHolder interface and implement it in holders

--- app/src/main/java/com/daniil/halushka/telegram/ui/screens/message_recycler_view/view/ViewTextMessage.kt
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/message_recycler_view/view/ViewTextMessage.kt
@@ -1,14 +1,14 @@
-package com.daniil.halushka.telegram.ui.screens.fragments.message_recycler_view.view
+package com.daniil.halushka.telegram.ui.screens.message_recycler_view.view
 
-data class ViewImageMessage(
+class ViewTextMessage(
     override val id: String,
     override val from: String,
     override val timeStamp: String,
-    override val fileUrl: String,
-    override val text: String = ""
+    override val fileUrl: String = "",
+    override val text: String
 ) : MessageView {
     override fun getTypeView(): Int {
-        return MessageView.MESSAGE_IMAGE
+        return MessageView.MESSAGE_TEXT
     }
 
     override fun equals(other: Any?): Boolean {
