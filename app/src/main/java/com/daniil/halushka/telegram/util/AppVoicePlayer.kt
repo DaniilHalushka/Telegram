6aafa63b1d0960379b7226971b9b2be06c2442d3
Daniil Halushka <popolzen22@gmail.com>
2024-05-18 23:12:30 +0300

    Create AppVoicePlayer

---
+++ app/src/main/java/com/daniil/halushka/telegram/util/AppVoicePlayer.kt
@@ -0,0 +1,21 @@
+package com.daniil.halushka.telegram.util
+
+import android.media.MediaPlayer
+import java.io.File
+
+class AppVoicePlayer {
+    private lateinit var moduleMediaPlayer: MediaPlayer
+    private lateinit var moduleFile: File
+
+    fun play() {
+
+    }
+
+    fun stop() {
+
+    }
+
+    fun realise() {
+
+    }
+}
