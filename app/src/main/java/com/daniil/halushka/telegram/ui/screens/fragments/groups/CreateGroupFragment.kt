77971723e5c68c2f5251bbca71b303a3614c992d
Daniil Halushka <popolzen22@gmail.com>
2024-06-02 11:58:01 +0300

    Work with plurals and quantity

--- app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/groups/CreateGroupFragment.kt
+++ app/src/main/java/com/daniil/halushka/telegram/ui/screens/fragments/groups/CreateGroupFragment.kt
@@ -10,6 +10,7 @@ import com.daniil.halushka.telegram.data.models.CommonModel
 import com.daniil.halushka.telegram.databinding.FragmentCreateGroupBinding
 import com.daniil.halushka.telegram.ui.screens.fragments.base.BaseFragment
 import com.daniil.halushka.telegram.util.APP_ACTIVITY
+import com.daniil.halushka.telegram.util.getPlurals
 import com.daniil.halushka.telegram.util.hideKeyboard
 import com.daniil.halushka.telegram.util.showToast
 
@@ -33,13 +34,14 @@ class CreateGroupFragment(private var listContacts: List<CommonModel>) :
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = getString(R.string.create_group)
-        APP_ACTIVITY.moduleAppDrawer.enableDrawer()
         hideKeyboard()
         initializeRecyclerView()
         createGroupBinding.createGroupButtonDone.setOnClickListener {
+            //TODO это для проверки
             showToast("HOORAY")
         }
         createGroupBinding.createGroupInputName.requestFocus()
+        createGroupBinding.createGroupCounts.text = getPlurals(listContacts.size)
     }
 
     private fun initializeRecyclerView() {
